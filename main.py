9fd1e50875c94b114ae8002fed288ff08ce1e98c
JEETDESAI25 <jeetdesai25@gmail.com>
2022-12-29 21:22:21 -0800

    add description,images and links

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
+
 st.set_page_config(layout="wide")
 
-col1, col2 = st.columns(2)
+col1, empty_col, col2 = st.columns([1.5, 0.5, 1.5])
 
 with col1:
     st.image("images/IMG_6664.png")
@@ -26,11 +27,18 @@ st.write(content2)
 col3, col4 = st.columns(2)
 
 df = pd.read_csv("data.csv", sep=";")
+
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[source code](https://github.com/JEETDESAI25?tab=repositories)")
 
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write("[source code](https://github.com/JEETDESAI25?tab=repositories)")
